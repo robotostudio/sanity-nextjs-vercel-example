bc225dc80a51216da81ece69da02bcbdc996708e
Jonathan Alford <jono@roboto.studio>
2021-11-21 11:43:12 +0000

    Upodated Next.js to 12 and upgraded all packages to latest and added correct npmClient to lerna.json

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,22 @@
 export default function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div
+      style={{
+        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "arial",
+      }}
+    >
+      <h1>Welcome to Next.js!</h1>
+      <p>
+        Forked from the official "Sanity + Next.js + Vercel example" by{" "}
+        <a href="https://roboto.studio" style={{ color: "#FF20C9" }}>
+          Roboto Studio
+        </a>
+      </p>
+    </div>
+  );
 }
